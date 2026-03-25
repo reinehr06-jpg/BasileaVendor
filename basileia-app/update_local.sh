3a75ba1835c3d46d42ff0c4e3e72118883be3777
Vinicius Reinehr <viniciusreinehr@MacBook-Air-de-Vinicius-5.local>
2026-03-25 10:34:34 -0300

    fix: update_local.sh paths

--- basileia-app/update_local.sh
+++ basileia-app/update_local.sh
@@ -2,6 +2,10 @@
 # Script para atualizar o ambiente local (migrations e cache)
 echo "🚀 Atualizando ambiente local Basileia Vendas..."
 
+# Determinar diretório do script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
 php artisan migrate --force
 php artisan config:clear
 php artisan route:clear
